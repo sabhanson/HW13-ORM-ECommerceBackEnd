57ac84df45bbbb9da74fb866e44e602b9fc79335
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-02 20:26:43 -0800

    set up all of the models and added the associations for product, category, tag, and producttag

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,7 @@
 // import important parts of sequelize library
+const database = require('mime-db');
 const { Model, DataTypes } = require('sequelize');
+const { Category } = require('.');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,6 +11,34 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: { isDecimal: true },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {isNumeric: true},
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id'
+      }
+    }
     // define columns
   },
   {
