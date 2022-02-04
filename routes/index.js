a0a9b0b7187b665eeda96440ba844156289a44c8
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-04 15:57:37 -0800

    still trying a tag

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ router.use('/api', apiRoutes);
 
 //this response will send if the user accesses a url that doesn't exist
 router.use((req, res) => {
-  res.send("<h1>This is not a valid URL path for the E-Commerce database</h1><h2>Try one of these links below:</h2><ul><li><a target=`_blank` href=`https://peaceful-fortress-89750.herokuapp.com/api/categories`>All Categories</a></li><li><a href=`https://peaceful-fortress-89750.herokuapp.com/api/products`>All Products</a></li><li><a href=`https://peaceful-fortress-89750.herokuapp.com/api/tags`>All Tags</a></li></ul>")
+  res.send("<h1>This is not a valid URL path for the E-Commerce database</h1><h2>Try one of these links below:</h2><ul><li><a target=`_blank` href=`/api/categories`>All Categories</a></li><li><a href=`https://peaceful-fortress-89750.herokuapp.com/api/products`>All Products</a></li><li><a href=`https://peaceful-fortress-89750.herokuapp.com/api/tags`>All Tags</a></li></ul>")
 });
 
 module.exports = router;
